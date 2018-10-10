c0d616b46013b11c69b1583dc430014422970125
zemenuyar <zemenuyar@gmail.com>
2018-10-10 16:19:49 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+         /*
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int input = keyboard.nextInt();
+        int sum = 0;
+
+        for (int i =0; i< input; i++){
+            sum = sum + i;
+            System.out.println(sum);
+        }
+
+        //backward this
+        sum = 0;
+        for (int i =input; i>=0; i--){
+            sum = sum + i;
+            System.out.println(sum);
+        }
+
+
+        String []colors = new  String[]{"blue", "orange", "red"};
+        for (int i=0; i<colors.length; i++)
+        System.out.println(colors[i]);
+    }
+    */
+
+    }
+
+}
